01b6bbdd6c6a111a5c9caacb4f8618157d079ef3
arynd <a.ryndenkov2001@yandex.ru>
2025-12-02 21:08:18 +0300

    Add UI

--- app/src/main/java/com/ryndenkov/notepad/presentation/screens/notes/NotesScreen.kt
+++ app/src/main/java/com/ryndenkov/notepad/presentation/screens/notes/NotesScreen.kt
@@ -1,22 +1,39 @@
 package com.ryndenkov.notepad.presentation.screens.notes
 
-import androidx.compose.foundation.ScrollState
-import androidx.compose.foundation.clickable
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.ScrollableTabRow
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.ryndenkov.notepad.domain.Note
+import com.ryndenkov.notepad.presentation.ui.theme.OtherNotesColors
+import com.ryndenkov.notepad.presentation.ui.theme.PinnedNotesColors
 
 @Composable
 fun NotesScreen(
@@ -24,24 +41,204 @@ fun NotesScreen(
     viewModel: NotesViewModel = viewModel()
 ) {
     val state by viewModel.state.collectAsState()
-
-    rememberScrollState()
-
-
-    Column(
-        modifier = Modifier
-            .padding(top = 48.dp)
-            .verticalScroll(rememberScrollState()),
-        verticalArrangement = Arrangement.spacedBy(8.dp)
+    LazyColumn(
+        modifier = modifier
+            .padding(top = 48.dp),
     ) {
-        state.otherNotes.forEach { note ->
-            Text(
-                modifier = Modifier.clickable {
-                    viewModel.processCommand(NotesCommands.EditNote(note))
-                },
-                text = "${note.title} - ${note.content}",
-                fontSize = 24.sp
+        item {
+
+            Title(
+                modifier = Modifier.padding(horizontal = 24.dp),
+                text = "All Notes"
             )
         }
+        item { Spacer(modifier = Modifier.height(16.dp)) }
+        item {
+            SearchBar(
+                modifier = Modifier.padding(horizontal = 24.dp),
+                query = state.query,
+                onQueryChange = {
+                    viewModel.processCommand(NotesCommands.InputSearchQuery(it))
+                }
+            )
+        }
+        item { Spacer(modifier = Modifier.height(24.dp)) }
+        item {
+            Subtitle(
+                modifier = Modifier.padding(horizontal = 24.dp),
+                text = "Pinned"
+            )
+        }
+        item { Spacer(modifier = Modifier.height(16.dp)) }
+        item {
+            LazyRow(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                horizontalArrangement = Arrangement.spacedBy(8.dp),
+                contentPadding = PaddingValues(horizontal = 24.dp)
+            ) {
+                itemsIndexed(
+                    items = state.pinnedNotes,
+                    key = { _, note -> note.id }) { index, note ->
+                    NoteCard(
+                        note = note,
+                        onNoteClick = {
+                            viewModel.processCommand(NotesCommands.EditNote(it))
+                        },
+                        onDoubleClick = {
+                            viewModel.processCommand(NotesCommands.DeleteNote(it.id))
+                        },
+                        onLongClick = {
+                            viewModel.processCommand(NotesCommands.SwitchPinnedStatus(it.id))
+                        },
+                        backgroundColor = PinnedNotesColors[index % PinnedNotesColors.size]
+                    )
+                }
+            }
+        }
+        item { Spacer(modifier = Modifier.height(24.dp)) }
+        item {
+            Subtitle(
+                modifier = Modifier.padding(horizontal = 24.dp), text = "Others"
+            )
+        }
+        item { Spacer(modifier = Modifier.height(16.dp)) }
+        itemsIndexed(
+            items = state.otherNotes,
+            key = { _, note -> note.id }) { index, note ->
+            NoteCard(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 24.dp),
+                note = note,
+                onNoteClick = {
+                    viewModel.processCommand(NotesCommands.EditNote(it))
+                },
+                onDoubleClick = {
+                    viewModel.processCommand(NotesCommands.DeleteNote(it.id))
+                },
+                onLongClick = {
+                    viewModel.processCommand(NotesCommands.SwitchPinnedStatus(it.id))
+                },
+                backgroundColor = OtherNotesColors[index % OtherNotesColors.size]
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+        }
     }
+}
+
+@Composable
+fun NoteCard(
+    modifier: Modifier = Modifier,
+    note: Note,
+    backgroundColor: Color,
+    onNoteClick: (Note) -> Unit,
+    onLongClick: (Note) -> Unit,
+    onDoubleClick: (Note) -> Unit,
+) {
+    Column(
+        modifier = modifier
+            .clip(RoundedCornerShape(16.dp))
+            .background(backgroundColor)
+            .combinedClickable(
+                onClick = {
+                    onNoteClick(note)
+                },
+                onLongClick = {
+                    onLongClick(note)
+                },
+                onDoubleClick = {
+                    onDoubleClick(note)
+                }
+            )
+            .padding(16.dp),
+    ) {
+        Text(
+            text = note.title,
+            fontSize = 14.sp,
+            color = MaterialTheme.colorScheme.onSurface
+        )
+        Spacer(modifier = Modifier.height(8.dp))
+        Text(
+            text = note.updatedAd.toString(),
+            fontSize = 12.sp,
+            color = MaterialTheme.colorScheme.onSurfaceVariant
+        )
+        Spacer(modifier = Modifier.height(24.dp))
+        Text(
+            text = note.content,
+            fontSize = 16.sp,
+            color = MaterialTheme.colorScheme.onSurface,
+            fontWeight = FontWeight.Medium
+        )
+    }
+
+}
+
+@Composable
+private fun Title(
+    modifier: Modifier = Modifier,
+    text: String
+) {
+    Text(
+        modifier = modifier,
+        text = text,
+        fontSize = 24.sp,
+        fontWeight = FontWeight.Bold,
+        color = MaterialTheme.colorScheme.onBackground
+    )
+}
+
+@Composable
+private fun SearchBar(
+    modifier: Modifier = Modifier,
+    query: String,
+    onQueryChange: (String) -> Unit
+) {
+    TextField(
+        modifier = modifier
+            .fillMaxWidth()
+            .border(
+                1.dp,
+                color = MaterialTheme.colorScheme.onSurfaceVariant,
+                shape = RoundedCornerShape(10.dp)
+            ),
+        value = query,
+        onValueChange = onQueryChange,
+        placeholder = {
+            Text(
+                text = "Search...",
+                fontSize = 14.sp,
+                color = MaterialTheme.colorScheme.onSurfaceVariant
+            )
+        },
+        colors = TextFieldDefaults.colors(
+            focusedContainerColor = MaterialTheme.colorScheme.surface,
+            unfocusedContainerColor = MaterialTheme.colorScheme.surface,
+            focusedIndicatorColor = Color.Transparent,
+            unfocusedIndicatorColor = Color.Transparent
+        ),
+        leadingIcon = {
+            Icon(
+                imageVector = Icons.Default.Search,
+                contentDescription = "Search Notes",
+                tint = MaterialTheme.colorScheme.onSurface
+            )
+        },
+        shape = RoundedCornerShape(10.dp)
+    )
+}
+
+@Composable
+private fun Subtitle(
+    modifier: Modifier = Modifier,
+    text: String
+) {
+    Text(
+        modifier = modifier,
+        text = text,
+        color = MaterialTheme.colorScheme.onSurfaceVariant,
+        fontWeight = FontWeight.Bold,
+        fontSize = 14.sp
+    )
 }
--- app/src/main/java/com/ryndenkov/notepad/presentation/ui/theme/Color.kt
+++ app/src/main/java/com/ryndenkov/notepad/presentation/ui/theme/Color.kt
@@ -2,10 +2,20 @@ package com.ryndenkov.notepad.presentation.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val White = Color(0xFFFFFFFF)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val Yellow100 = Color(0xFFFDF2E1)
+val Yellow200 = Color(0xFFFCE5C3)
+val Blue100 = Color(0xFFE3EBF2)
+val Blue200 = Color(0xFFC8D8E5)
+val Green = Color(0xFFD1EAD1)
+val Purple = Color(0xFFDACEE6)
+
+val Grey100 = Color(0xFFF9F9F9)
+val Grey200 = Color(0xFF868484)
+val Grey300 = Color(0xFF3D3B3B)
+
+val Brown = Color(0xFF703410)
+
+val PinnedNotesColors = listOf(Yellow200, Purple, Blue200)
+val OtherNotesColors = listOf(Yellow100, Green, Blue100)
